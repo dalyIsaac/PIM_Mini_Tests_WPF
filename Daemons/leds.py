4db4287e481f077602d3e790071788e3c573ef5b
DalyIsaac <isaac.daly@outlook.com>
2018-02-13 09:58:54 +1300

    More logging for LEDs and user inputs

--- Daemons/leds.py
+++ Daemons/leds.py
@@ -18,34 +18,34 @@ IED_DATA_RX = GPIO(pin=4, direction=OUT)
 
 def test_ccp_ok(level, logging):
     """Tests that the CCP OK LED can be turned on"""
-    #return _test(CCP_OK, level)
+    #return _test(CCP_OK, level, logging)
     return _write(CCP_OK, level, logging)
 
 def test_ied_ok(level, logging):
     """Tests that the IED OK LED can be turned on"""
-    #return _test(IED_OK, level)
+    #return _test(IED_OK, level, logging)
     return _write(IED_OK, level, logging)
 
 def test_fault(level, logging):
     """Tests that the Fault LED can be turned on"""
-    #return _test(FAULT, level)
+    #return _test(FAULT, level, logging)
     return _write(FAULT, level, logging)
 
-def test_ccp_data_tx(level):
+def test_ccp_data_tx(level, logging):
     """Tests that the CCP Data Tx (transmit) LED can be turned on"""
-    return _test(CCP_DATA_TX, level)
+    return _test(CCP_DATA_TX, level, logging)
 
-def test_ccp_data_rx(level):
+def test_ccp_data_rx(level, logging):
     """Tests that the CCP Data Rx (receive) LED can be turned on"""
-    return _test(CCP_DATA_RX, level)
+    return _test(CCP_DATA_RX, level, logging)
 
-def test_ied_data_tx(level):
+def test_ied_data_tx(level, logging):
     """Tests that the IED Data Tx (transmit) LED can be turned on"""
-    return _test(IED_DATA_TX, level)
+    return _test(IED_DATA_TX, level, logging)
 
-def test_ied_data_rx(level):
+def test_ied_data_rx(level, logging):
     """Tests that the IED Data Rx (receive) LED can be turned on"""
-    return _test(IED_DATA_RX, level)
+    return _test(IED_DATA_RX, level, logging)
 
 def _write(pin, level, logging):
     try:
@@ -56,8 +56,18 @@ def _write(pin, level, logging):
         logging.error(ex)
         return False
 
-def _test(pin, level):
-    pin.write(level)
-    if pin.read() is level:
-        return True
-    return False
+def _test(pin, level, logging):
+    try:
+        pin.write(level)
+        logging.debug("Value written")
+        val = pin.read()
+        if val is level:
+            self.logging.debug("Value read is the one same as the one written")
+            return True
+        else:
+            message = "Value read is different to the one written: " + str(val) + " != " + str(level)
+            self.logging.fatal()
+            return False
+    except Exception as ex:
+        self.logging.fatal(ex)
+        return False
