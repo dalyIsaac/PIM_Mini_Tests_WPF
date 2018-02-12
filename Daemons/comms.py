b0a8f3c267bc23e70676c6c4364ac6d7395c4e65
DalyIsaac <isaac.daly@outlook.com>
2018-02-13 11:34:59 +1300

    Updated comms names

--- Daemons/comms.py
+++ Daemons/comms.py
@@ -3,23 +3,23 @@
 from serial import Serial, rs485, PARITY_ODD, STOPBITS_ONE, EIGHTBITS
 
 CCP_TTL = Serial(baudrate=9600, parity=PARITY_ODD, stopbits=STOPBITS_ONE, bytesize=EIGHTBITS)
-CCP_TTL.port = "/dev/ttymx2"
+CCP_TTL.port = "/dev/ttymxc2"
 
 CCP_RS232 = Serial(baudrate=9600, parity=PARITY_ODD, stopbits=STOPBITS_ONE, bytesize=EIGHTBITS)
-CCP_RS232.port = "/dev/ttymx2"
+CCP_RS232.port = "/dev/ttymxc2"
 
 CCP_RS485 = Serial(baudrate=9600, parity=PARITY_ODD, stopbits=STOPBITS_ONE, bytesize=EIGHTBITS)
-CCP_RS485.port = "/dev/ttymx2"
+CCP_RS485.port = "/dev/ttymxc2"
 CCP_RS485.rs485_mode = rs485.RS485Settings()
 
 IED_TTL = Serial(baudrate=9600, parity=PARITY_ODD, stopbits=STOPBITS_ONE, bytesize=EIGHTBITS)
-IED_TTL.port = "/dev/ttymx4"
+IED_TTL.port = "/dev/ttymxc4"
 
 IED_RS232 = Serial(baudrate=9600, parity=PARITY_ODD, stopbits=STOPBITS_ONE, bytesize=EIGHTBITS)
-IED_RS232.port = "/dev/ttymx4"
+IED_RS232.port = "/dev/ttymxc4"
 
 IED_RS485 = Serial(baudrate=9600, parity=PARITY_ODD, stopbits=STOPBITS_ONE, bytesize=EIGHTBITS)
-IED_RS485.port = "/dev/ttymx4"
+IED_RS485.port = "/dev/ttymxc4"
 
 TEST_STRING = b"The quick brown fox jumps over the lazy dog 0123456789"
 
